316dc7cbbcf96b3d4103bfb588895cb883af7131
romark99 <romark@tut.by>
2018-04-29 01:00:58 +0300

    Added error handlers.

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ import {createUser} from "./domGenerator";
 
 function func() {
     let userLogin = document.getElementById("nickname").value;
-    fetch('https://api.github.com/users/'+userLogin)
+    fetch('https://api.github.com/users/' + userLogin)
         .then(function (response) {
-            return response.json();
+            if (response.status > 100 && response.status <= 400)
+                return response.json();
+            else if (response.status === 404) {
+                throw new Error("NOT FOUND");
+            }
+            else if (response.status > 400 && response.status < 500) {
+                throw new Error("SOME CLIENT ERROR");
+            }
+            else if (response.status > 500 && response.status < 600) {
+                throw new Error("SOME SERVER ERROR");
+            }
+            else {
+                throw new Error("UNEXPECTED STATE");
+            }
         })
-        .then(user=>createUser(user));
+        .then(user => createUser(user))
+        .catch((e)=>{
+        let h1 = document.createElement("h1");
+        h1.innerText = e.toString();
+        document.body.appendChild(h1);
+    });
 }
 
 document.getElementById("btnSubmit").addEventListener("click", func);
