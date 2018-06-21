d0105dc1c27686011bf67f1c7653a2780c98a92c
romark99 <romark@tut.by>
2018-06-21 12:03:59 +0300

    Improving.

--- src/Request/fetchAdditional.js
+++ src/Request/fetchAdditional.js
@@ -3,49 +3,21 @@ import {makeErrorPage} from "../AppError";
 import {getStateFromStore} from "../index";
 import isError from "./isError"
 
-// function getAdditional(name) {
-//     return 'https://api.github.com/users/' + name + '/orgs';
-// }
-//
-// let funcAddit = (what)=> {
-//     let foo;
-//     switch (what) {
-//         case 'ADDITIONALLY':
-//             foo = (name) => 'https://api.github.com/users/' + name + '/orgs';
-//             break;
-//         case 'FOLLOWERS':
-//             foo = (name) => 'https://api.github.com/users/' + name + '/followers';
-//             break;
-//         case 'REPOS':
-//             foo = (name) => 'https://api.github.com/users/' + name + '/repos';
-//             break;
-//     }
-//     let state = getStateFromStore();
-//     fetch(foo(state.reducerUser.user.login))
-//         .then(response=>isError(response))
-//         .then(responseJSON => {
-//             store.dispatch({
-//                 type: what,
-//                 list: responseJSON
-//             });
-//         }).catch(e => makeErrorPage(e));
-// };
-
-let funcAdditional = (what) => {
-    let state = getStateFromStore();
-    let url;
-    switch (what) {
-        case 'ADDITIONALLY':
-            url = state.reducerUser.user.organizations_url;
-            break;
-        case 'FOLLOWERS':
-            url = state.reducerUser.user.followers_url;
-            break;
-        case 'REPOS':
-            url = state.reducerUser.user.repos_url;
-            break;
-    }
+const funcAll = (what) => {
     return function () {
+        let state = getStateFromStore();
+        let url;
+        switch (what) {
+            case 'ADDITIONALLY':
+                url = state.reducerUser.user.organizations_url;
+                break;
+            case 'FOLLOWERS':
+                url = state.reducerUser.user.followers_url;
+                break;
+            case 'REPOS':
+                url = state.reducerUser.user.repos_url;
+                break;
+        }
         fetch(url)
             .then(response=>isError(response))
             .then(responseJSON => {
@@ -57,4 +29,8 @@ let funcAdditional = (what) => {
     }
 };
 
-export {funcAdditional};
+const funcAddit = funcAll('ADDITIONALLY');
+const funcFollowers = funcAll('FOLLOWERS');
+const funcRepos = funcAll('REPOS');
+
+export {funcAddit, funcFollowers, funcRepos};
