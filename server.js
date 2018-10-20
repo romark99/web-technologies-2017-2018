b6eb90341b63d34c8b9ad720fcdfe8ea059ff3fd
romark99 <romark999@gmail.com>
2018-10-20 21:04:20 +0300

    Added routes.

--- server.js
+++ server.js
@@ -16,6 +16,36 @@ app.get('/movies/:id', function (req, res) {
     res.send(movie);
 });
 
+app.get('/search/:substring', function (req, res) {
+    console.log(req.params);
+    const foundMovies = movies.filter(function (movie) {
+        return movie.title.includes(req.params.substring);
+    });
+    res.send(foundMovies);
+});
+
+//  query args: limit and offset
+app.get('/pagination', function (req, res) {
+    console.log(req.query);
+    const offset = Number(req.query.offset);
+    const limit = Number(req.query.limit);
+    const moviesOnPage = movies.slice(offset, offset + limit);
+    res.send(moviesOnPage);
+});
+
+// query args: field and direction (asc, desc)
+app.get('/sort', function (req, res) {
+    console.log(req.query);
+    const field = req.query.field;
+    const direction = req.query.direction;
+    const sortedMovies = movies.sort((a, b) => {
+        if (a[field] > b[field]) return 1;
+        if (a[field] < b[field]) return -1;
+        return 0;
+    });
+    res.send(direction === 'asc' ? sortedMovies : (direction === 'desc' ? sortedMovies.reverse() : [] ));
+});
+
 app.get('/', function (req, res) {
     res.send('Hello API');
 });
