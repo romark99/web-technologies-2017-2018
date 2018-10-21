e4b7aa33c07b314c9db5bc3aba8fd323f7fddfb2
romark99 <romark999@gmail.com>
2018-10-21 12:01:27 +0300

    Added routes.

---
+++ src/routes/index.js
@@ -0,0 +1,57 @@
+const movies = require('../data');
+
+const express  = require('express');
+
+function getRouter() {
+    const router = express.Router();
+
+    router.get('/movies', function (req, res) {
+        res.send(movies);
+    });
+
+    router.get('/movies/:id', function (req, res) {
+        console.log(req.params);
+        const movie = movies.find(function (movie) {
+            return movie.id === Number(req.params.id);
+        });
+        res.send(movie);
+    });
+
+    router.get('/search/:substring', function (req, res) {
+        console.log(req.params);
+        const foundMovies = movies.filter(function (movie) {
+            return movie.title.includes(req.params.substring);
+        });
+        res.send(foundMovies);
+    });
+
+    //  query args: limit and offset
+    router.get('/pagination', function (req, res) {
+        console.log(req.query);
+        const offset = Number(req.query.offset);
+        const limit = Number(req.query.limit);
+        const moviesOnPage = movies.slice(offset, offset + limit);
+        res.send(moviesOnPage);
+    });
+
+    // query args: field and direction (asc, desc)
+    router.get('/sort', function (req, res) {
+        console.log(req.query);
+        const field = req.query.field;
+        const direction = req.query.direction;
+        const sortedMovies = movies.sort((a, b) => {
+            if (a[field] > b[field]) return 1;
+            if (a[field] < b[field]) return -1;
+            return 0;
+        });
+        res.send(direction === 'asc' ? sortedMovies : (direction === 'desc' ? sortedMovies.reverse() : []));
+    });
+
+    router.get('/', function (req, res) {
+        res.send('Hello API');
+    });
+
+    return router;
+}
+
+module.exports = getRouter;
