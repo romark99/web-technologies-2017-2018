04f7f540135b3550ce916ebfe1c01d1181d23a3d
romark99 <romark999@gmail.com>
2018-10-22 19:21:37 +0300

    Added Joi validation.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,16 @@
+const Joi = require('joi');
 const services = require('../services')();
 const constants = require('../constants');
 
 function getControllers() {
+
+    const isError = (res, error) => {
+        if (error && error.details) {
+            res.status(400).json(error);
+            return true;
+        }
+        return false;
+    };
 
     const getMovies = (req, res) => res.send(
         services.getMovies()
@@ -9,32 +18,64 @@ function getControllers() {
 
     const getMovieById = (req, res) => {
         console.log(req.params);
-        res.send(
-            services.getMovieById(Number(req.params.id))
-        );
+        const schema = Joi.number().min(0).required();
+        const {value, error} = Joi.validate(req.params.id, schema);
+        if (!isError(res, error)) {
+            res.send(
+                services.getMovieById(Number(req.params.id))
+            );
+        }
     };
 
     const getMoviesBySubstring = (req, res) => {
         console.log(req.params);
-        res.send(
-            services.getMoviesBySubstring(req.params.substring)
-        );
+        const schema = Joi.string();
+        const {value, error} = Joi.validate(req.params.substring, schema);
+        if (!isError(res,error)) {
+            res.send(
+                services.getMoviesBySubstring(req.params.substring)
+            );
+        }
     };
 
     const getPagination = (req, res) => {
         console.log(req.query);
-        const offset = Number(req.query.offset);
-        const limit = Number(req.query.limit);
-        res.send(
-            services.getPagination(offset, limit)
-        );
+        const schema = Joi.object().keys({
+            offset: Joi.number()
+                .integer()
+                .min(0)
+                .required(),
+            limit: Joi.number()
+                .integer()
+                .min(1)
+                .max(100)
+                .required()
+        });
+        const {value, error} = Joi.validate(req.query, schema);
+        if (!isError(res, error)) {
+            const offset = Number(req.query.offset);
+            const limit = Number(req.query.limit);
+            res.send(
+                services.getPagination(offset, limit)
+            );
+        }
     };
 
     const sortMovies = (req, res) => {
         console.log(req.query);
-        res.send(
-            services.sortMovies(req.query.field, req.query.direction)
-        );
+        const schema = Joi.object().keys({
+            field: Joi.string()
+                .required(),
+            direction: Joi.string()
+                .valid(constants.DIRECTION_SORT_ASC, constants.DIRECTION_SORT_DESC)
+                .required()
+        });
+        const {value, error} = Joi.validate(req.query, schema);
+        if (!isError(res, error)) {
+            res.send(
+                services.sortMovies(req.query.field, req.query.direction)
+            );
+        }
     };
 
     const helloApi = (req, res) => res.send(constants.HELLO_API);
