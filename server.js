3e49eefc2794e69c8fccc182fd0b5e01434cb721
romark99 <romark999@gmail.com>
2018-10-22 20:00:45 +0300

    Added graceful shutdown.

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const app = express();
 
 app.use(router);
 
-app.listen(process.env.HTTP_PORT, function() {
+const server = app.listen(process.env.HTTP_PORT, function() {
   console.log(constants.API_APP_STARTED);
 });
+
+function gracefulShutdown() {
+  console.info(constants.TERMINATION_SIGNAL_RECIEVED);
+  console.log(constants.CLOSING_SERVER);
+  server.close(() => {
+    console.log(constants.SERVER_CLOSED);
+    process.exit(0);
+  });
+}
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
